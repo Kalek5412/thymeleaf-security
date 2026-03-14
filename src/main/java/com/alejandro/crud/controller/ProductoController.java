c9164786efefad96493fef9923d61a44caab2677
kalek5412 <alejandrolujanllauri@gmail.com>
2026-03-14 12:55:21 -0500

    3

--- src/main/java/com/alejandro/crud/controller/ProductoController.java
+++ src/main/java/com/alejandro/crud/controller/ProductoController.java
@@ -9,13 +9,13 @@ import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/producto")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ProductoController {
 
     @Autowired
