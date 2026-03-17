d02ed27f7caff17c96d747bd913b0a5260c3be31
kalek5412 <alejandrolujanllauri@gmail.com>
2026-03-17 18:25:08 -0500

    5

--- src/main/java/com/alejandro/crud/entity/Producto.java
+++ src/main/java/com/alejandro/crud/entity/Producto.java
@@ -1,12 +1,10 @@
 package com.alejandro.crud.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "productos")
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
