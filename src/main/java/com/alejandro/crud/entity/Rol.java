052c280d348cd2e4e0f172298878cc07e7cc2c10
kalek5412 <alejandrolujanllauri@gmail.com>
2026-03-23 17:54:57 -0500

    casi termiando

---
+++ src/main/java/com/alejandro/crud/entity/Rol.java
@@ -0,0 +1,40 @@
+package com.alejandro.crud.entity;
+
+import com.alejandro.crud.enums.RolNombre;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "roles")
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true)
+    private RolNombre rolNombre;
+
+    public Rol() {
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+}
