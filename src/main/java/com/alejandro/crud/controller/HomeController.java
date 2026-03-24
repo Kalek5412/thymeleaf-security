95ab4800162d5b7d4c3947141fafff34f24900d1
kalek5412 <alejandrolujanllauri@gmail.com>
2026-03-24 17:58:58 -0500

    7

--- src/main/java/com/alejandro/crud/controller/HomeController.java
+++ src/main/java/com/alejandro/crud/controller/HomeController.java
@@ -7,8 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping(value = {"/","index"})
+    @GetMapping(value = {"/", "/index"})
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/forbidden")
+    public String forbidden(){
+        return "forbidden";
     }
 }
